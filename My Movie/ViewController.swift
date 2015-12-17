73c5fbcba7ab6962ea6915af7a440793e066f654
Andi Setiyadi <setiyadi@us.ibm.com>
2015-12-16 22:38:58 -0700

    Initial Commit

---
+++ My Movie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Movie
+//
+//  Created by Andi Setiyadi on 12/16/15.
+//  Copyright © 2015 PFI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
